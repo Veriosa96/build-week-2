1df81c577f81e177801ca1b67b97f111a3dc4186
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-02-01 16:58:56 +0100

    Revert "add js"
    This reverts commit 7ffecb2afd4f3ebce8ccbcd6e05971f80c337f14.

--- assets/js/album.js
+++
@@ -1,8 +0,0 @@
-// devo inserire url
-let url = window.location.search;
-
-// creo con urlsearch per il collegamento url del risultato del quiz
-const parametro = new URLSearchParams(url);
-
-// mi prendo dall'url il vaore result
-const id = parametro.get("idAlbum");
