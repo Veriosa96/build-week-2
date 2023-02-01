1de886f59117686d0f6040e94ad9e20f699bc344
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-02-01 12:40:51 +0100

    add js frist and second sections

---
+++ assets/js/javascript.js
@@ -0,0 +1,68 @@
+let api = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
+const fetchSongs = async function (query) {
+  let res = await fetch(`${api}${query}`);
+  let { data: song } = await res.json();
+  return song;
+};
+
+const mainSong = async () => {
+  let canzone = await fetchSongs("dumb");
+  let div = document.getElementById("dataTrack");
+  let favSong = canzone[2];
+  div.innerHTML += `
+  <img class="img py-3" src="${favSong.album.cover_medium}" style="width: 150px"/>
+  <div class="mainAlbum">
+    <p>${favSong.album.title}</p>
+    <div class="bodyCard">
+        <h1>${favSong.title}</h1>
+        <p>${favSong.artist.name}</p>
+        <p>Ascolta il nuovo singolo di</p>
+        <div class="buttonCard">
+            <ul>
+                <li class="firstButton">Play</li>
+                <li class="secondButton">Salva</li>
+                <i class="bi bi-three-dots"></i>
+            </ul>
+        </div>
+    </div>
+  </div>
+  `;
+};
+/* FIRST SECTION */
+const nascondi = () => {
+  let element = document.querySelectorAll(".box");
+  console.log(element);
+  element[0].classList.toggle("d-none");
+};
+
+let bottone = document.getElementById("hidden");
+
+bottone.addEventListener("click", () => {
+  nascondi();
+});
+
+/* SECOND SECTION */
+const secondSection = async () => {
+  let canzoni = await fetchSongs("track");
+  let second = document.getElementById("second");
+  let favSongs = [
+    canzoni[6],
+    canzoni[1],
+    canzoni[2],
+    canzoni[3],
+    canzoni[4],
+    canzoni[5]
+  ];
+  favSongs.forEach(({ title_short, album }) => {
+    second.innerHTML += `<div class="card col" id="second-section">
+      <img src="${album.cover_small}" alt="image">
+      <div class="card-body">
+        <p class="card-title">${title_short}</p>
+      </div>`;
+  });
+};
+
+window.onload = async function () {
+  await mainSong();
+  await secondSection();
+};
