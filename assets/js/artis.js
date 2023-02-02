678aab36dccefb1abc2f784f20601585f9b4d676
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-02-02 10:50:10 +0100

    js artist page

--- assets/js/artis.js
+++ assets/js/artis.js
@@ -8,45 +8,84 @@ const parametro = new URLSearchParams(url);
 const id = parametro.get("idArtist");
  */
 
-/*
-const aritst = async () => {
-  try {
-    let res = await fetch(
-      "https://striveschool-api.herokuapp.com/api/deezer/artist/415"
-    );
-    if (res.ok) {
-      let data = await res.json();
-      let track = data.data;
-      // let artist = track.contributors[0];
-      let bodyCard = document.getElementById("cardPageArtist");
-      console.log(data);
+let id = "415";
+let api = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
+let endpoint = "/top?limit=50";
+const fetchArtist = async () => {
+  let res = await fetch(api + id);
+  let artist = await res.json();
+  return artist;
+};
 
-      bodyCard.innerHTML = `
+let artist = async () => {
+  let artista = await fetchArtist();
+  let bodyCard = document.getElementById("cardPageArtist");
+  bodyCard.innerHTML = `
         <div class="card border-0 text-bg-dark p-0">
         <img id="cardOverlay"
-          src="${track.picture_big}"
+          src="${artista.picture_big}"
           class="card-img rounded-0" alt="..." />
         <div class="card-img-overlay d-flex flex-column justify-content-between justify-content-md-end textBox">
           <div class="arrow arrowMobile d-flex justify-content-center align-items-center">
-            <i class="bi bi-chevron-left"></i>
+            <i class="bi bi-chevron-left d-flex d-md-none"></i>
           </div>
           <p class="hiddenMobile">
             <i class="bi bi-patch-check-fill me-2 hiddenMobile"></i><small>Artista verificato</small>
           </p>
-          <h5 class="card-title">${track.name}</h5>
+          <h5 class="card-title">${artista.name}</h5>
           <p class="card-text downMobile">
-            <small>3.433.168 ascoltatori mensili</small>
+            <small>${artista.nb_fan} ascoltatori mensili</small>
           </p>
         </div>
       </div>
-        
         `;
+};
+
+let tracks = async () => {
+  try {
+    let res = await fetch(api + id + endpoint);
+    if (res.ok) {
+      let data = await res.json();
+      let tracks = data.data;
+      let div = document.getElementById("cardPageTracks");
+      for (let i = 0; i < tracks.length; i++) {
+        let numb = i + 1;
+        let durata = [];
+        durata.push(tracks[i].duration);
+        let stringDurata = durata[0].toString();
+        let pippo = stringDurata.split("");
+        pippo.splice(1, 0, ":");
+        let finalDurata = pippo.join("");
+
+        console.log(finalDurata);
+        div.innerHTML += `
+        <div class="col d-flex justify-content-between align-items-center hoverArtist">
+          <div class="left d-flex align-items-center">
+            <p class="me-3">${numb}</p>
+            <img id="smallCover" src="${tracks[i].album.cover_small}" alt=""
+                class="me-3" />
+            <div>
+              <p class="noneMobile albumName">${tracks[i].title}</p>
+              <p class="noneDesktop">276.968.994</p>
+            </div>
+          </div>
+          <div class="center">
+            <p>${tracks[i].rank}</p>
+          </div>
+          <div class="right">
+            <p class="">${finalDurata}</p>
+            <i class="bi bi-three-dots"></i>
+          </div>
+        </div>
+        `;
+      }
     }
   } catch (error) {
-    console.log(error);
+    console.log("error");
   }
 };
 
-aritst();
-
-*/
+window.onload = async function () {
+  artist();
+  tracks();
+};
